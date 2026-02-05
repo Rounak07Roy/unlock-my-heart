5f3459d050f438406594cecb0904c2bd35e12619
Rounak Roy <21052271@kiit.ac.in>
2026-02-05 17:21:15 +0530

    update 2

--- src/theme.ts
+++
@@ -1,7 +0,0 @@
-export type Theme = "purple" | "pink" | "sunset";
-
-export const themes = {
-  purple: "from-purple-900 via-indigo-900 to-black",
-  pink: "from-pink-300 via-rose-200 to-purple-300",
-  sunset: "from-orange-400 via-pink-500 to-purple-600",
-};
